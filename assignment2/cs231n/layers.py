08b3aa355a96bb753956f0097cab5b489f5424bb
wywongbd <emarald10@gmail.com>
2017-10-22 13:08:56 +0800

    fixed dropout forward

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -339,7 +339,7 @@ def dropout_forward(x, dropout_param):
         # TODO: Implement training phase forward pass for inverted dropout.   #
         # Store the dropout mask in the mask variable.                        #
         #######################################################################
-        mask = (np.random.rand(*x.shape) < p).astype(int) / p
+        mask = (np.random.rand(*x.shape) > p).astype(int) / p
         out = mask * x
         #######################################################################
         #                           END OF YOUR CODE                          #
